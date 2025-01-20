4b195cde0718195e5dcf44435260863d7c2844a8
krystlc <alex.valle@gmail.com>
2025-01-20 13:17:44 -0500

    feat: add redis

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,26 @@
 import { Application, Router } from "https://deno.land/x/oak@v17.1.4/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import { BirdEyeClient } from "./services/birdeye/BirdEyeClient.ts";
 import "jsr:@std/dotenv/load";
 
 const router = new Router();
-const client = new BirdEyeClient();
 
 router.get("/toptraders", async (context) => {
-  const traders = await client.getTopTraders();
-  console.log(`Fetched ${traders.length} traders, sending to Discord...`);
-  context.response.body = traders;
+  /**
+   *
+   * get analyzed list of traders from redis
+   */
+  context.response.body = [];
 });
 
-router.post("/process-message", async (context) => {
-  const tokens = await client.getTokenList({
-    sortBy: "",
-    sortType: "",
-    offset: 0,
-    limit: 0,
-    minLiquidity: 0,
-  });
-  console.log(`Fetched ${tokens.length} tokens, sending to Discord...`);
-  context.response.body = tokens;
+router.post("/analyze", async (context) => {
+  /**
+   *
+   * 1. get trending tokens
+   * 2. get top traders per volume of those tokens
+   * 3. get and store history of those traders
+   * 4. loop through top traders and call n8n to analyze each one posting its results in redis
+   */
+  context.response.status = 200;
 });
 
 const app = new Application();
